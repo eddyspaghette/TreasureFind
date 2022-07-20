8b0720caa86a328e2365f1a8620f22c1e9c6047f
rmp9 <57851775+raizapw19@users.noreply.github.com>
2022-07-19 23:32:59 -0700

    Add User Profile Activity

---
+++ app/src/main/java/com/AERYZ/treasurefind/profile/UserProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.AERYZ.treasurefind.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
+import com.AERYZ.treasurefind.R
+
+class UserProfileActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
