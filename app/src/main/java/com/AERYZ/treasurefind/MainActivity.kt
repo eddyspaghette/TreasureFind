c2c1dd5860c72c532aace9475321b6320ebe70e9
akn-sfu <magicmonster17@gmail.com>
2022-07-19 19:31:06 -0700

    Fix login fragment

--- app/src/main/java/com/AERYZ/treasurefind/MainActivity.kt
+++ app/src/main/java/com/AERYZ/treasurefind/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val loginFragment = LoginFragment()
-        supportFragmentManager.beginTransaction().replace(R.id.frag_container,loginFragment, "login").commit()
+        val fragment = LoginFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.frag_container,fragment, "login").commit()
     }
 }
