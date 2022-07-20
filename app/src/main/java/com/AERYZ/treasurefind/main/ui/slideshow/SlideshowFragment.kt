caf38b0d93fb1877aad496d8b5f0dce66210cca0
rmp9 <57851775+raizapw19@users.noreply.github.com>
2022-07-20 12:19:26 -0700

    Edit fragment

--- app/src/main/java/com/AERYZ/treasurefind/main/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/AERYZ/treasurefind/main/ui/slideshow/SlideshowFragment.kt
@@ -28,10 +28,6 @@ class SlideshowFragment : Fragment() {
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
