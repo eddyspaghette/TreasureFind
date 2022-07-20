8f93a5111cfc86a383bb7474d88d0ae5c3689a10
AbbyLan <lindalan4891@gmail.com>
2022-07-20 00:16:17 -0700

    init hide treasures activity page

--- app/src/main/java/com/AERYZ/treasurefind/GameStartFragment.kt
+++ app/src/main/java/com/AERYZ/treasurefind/GameStartFragment.kt
@@ -34,7 +34,7 @@ class GameScopeSelectFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_scope_select, container, false)
+        return inflater.inflate(R.layout.fragment_game_start, container, false)
     }
 
     companion object {
