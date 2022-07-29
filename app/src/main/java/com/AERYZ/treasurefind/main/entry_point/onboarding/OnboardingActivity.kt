cd50174c234c6c27d0a022bc9bbdea83354e9c61
rmp9 <raiza.paragguawana@gmail.com>
2022-07-28 18:16:49 -0700

    add basic viewpager
    Signed-off-by: rmp9 <raiza.paragguawana@gmail.com>

---
+++ app/src/main/java/com/AERYZ/treasurefind/main/entry_point/onboarding/OnboardingActivity.kt
@@ -0,0 +1,41 @@
+package com.AERYZ.treasurefind.main.entry_point.onboarding
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.AERYZ.treasurefind.R
+import com.AERYZ.treasurefind.main.entry_point.MainActivity
+import com.google.android.material.tabs.TabLayout
+
+class OnboardingActivity: AppCompatActivity() {
+    var onboardingAdapter: OnboardingAdapter? = null
+    var tabLayout: TabLayout? = null
+    var onboardingViewPager: ViewPager? = null
+    var skipBtn: Button? = null
+    var next: TextView? = null
+    var position = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+
+        val onboardingData:MutableList<OnboardingData> = ArrayList()
+
+        onboardingData.add(OnboardingData("title1", "desc1", R.drawable.pic1))
+        onboardingData.add(OnboardingData("title2", "desc2", R.drawable.pic2))
+        onboardingData.add(OnboardingData("title3", "desc3", R.drawable.pic3))
+
+        setOnboardingAdapter(onboardingData)
+    }
+
+    private fun setOnboardingAdapter(onboardingData: List<OnboardingData>) {
+        onboardingViewPager = findViewById(R.id.pager)
+        onboardingAdapter = OnboardingAdapter(this, onboardingData)
+        onboardingViewPager!!.adapter = onboardingAdapter
+        tabLayout?.setupWithViewPager(onboardingViewPager)
+
+    }
+}
