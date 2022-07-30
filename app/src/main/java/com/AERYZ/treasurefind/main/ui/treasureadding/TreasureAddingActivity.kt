9b1f3a35ce41ca821ae7344ffb6a0d9df1b0bc9e
Edward Zhou <eddyspaghette@gmail.com>
2022-07-29 17:37:10 -0700

    add progress bar and upload image functionality to firebase storage

--- app/src/main/java/com/AERYZ/treasurefind/main/ui/treasureadding/TreasureAddingActivity.kt
+++ app/src/main/java/com/AERYZ/treasurefind/main/ui/treasureadding/TreasureAddingActivity.kt
@@ -1,15 +1,28 @@
 package com.AERYZ.treasurefind.main.ui.treasureadding
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.AERYZ.treasurefind.R
 import com.AERYZ.treasurefind.main.CameraModule
+import com.google.android.material.progressindicator.CircularProgressIndicator
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
 
 class TreasureAddingActivity : AppCompatActivity() {
     private lateinit var treasureAddingViewModel: TreasureAddingViewModel
+    private lateinit var progressBar: CircularProgressIndicator
+    private var storage = Firebase.storage
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_treasure_adding)
@@ -20,6 +33,8 @@ class TreasureAddingActivity : AppCompatActivity() {
             // everytime the bitmap changes, set the imageview
             treasureImageView.setImageBitmap(it)
         }
+        progressBar = findViewById(R.id.progress_bar)
+        progressBar.visibility = View.INVISIBLE
 
         setButtonListeners()
 
@@ -33,8 +48,29 @@ class TreasureAddingActivity : AppCompatActivity() {
 
     private fun addTreasureBtnListener() {
         val btn: Button = findViewById(R.id.addButton)
+        val titleView: TextView = findViewById(R.id.treasureTitle)
+        val descriptionView: TextView = findViewById(R.id.treasureDescription)
         btn.setOnClickListener {
+            val uid = FirebaseAuth.getInstance().currentUser?.uid
+            if (uid != null) {
+                val baos = ByteArrayOutputStream()
+                treasureAddingViewModel.treasurePhoto.value!!.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+                val data = baos.toByteArray()
 
+                val treasureRef = storage.reference
+                    .child("images/treasures/uid/${uid}/${System.currentTimeMillis()}")
+
+                val uploadTask = treasureRef.putBytes(data)
+                uploadTask.addOnFailureListener {
+                    Log.d("DEBUG: failed upload", "$it")
+                }.addOnSuccessListener {
+                    progressBar.visibility = View.INVISIBLE
+                    Toast.makeText(this, "Uploaded Successfully", Toast.LENGTH_SHORT)
+                    Log.d("DEBUG: uploaded successfully", "$it")
+                }.addOnProgressListener {
+                    progressBar.visibility = View.VISIBLE
+                }
+            }
         }
     }
 
