331b595853dd415b9d620755684a4f174e6cc17f
Edward Zhou <eddyspaghette@gmail.com>
2022-07-29 18:52:45 -0700

    delete treasure.kt and add firebase interface

---
+++ app/src/main/java/com/AERYZ/treasurefind/db/MyFirebase.kt
@@ -0,0 +1,8 @@
+package com.AERYZ.treasurefind.db
+
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
+class MyFirebase {
+    private var storage = Firebase.storage
+}
--- app/src/main/java/com/AERYZ/treasurefind/db/Treasure.kt
+++
@@ -1,7 +0,0 @@
-package com.AERYZ.treasurefind.db
-
-import android.net.Uri
-
-
-class Treasure(var title: String, var desc: String) {
-}
