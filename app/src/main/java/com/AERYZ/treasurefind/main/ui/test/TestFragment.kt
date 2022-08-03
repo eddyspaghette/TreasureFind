a0d1bdfcdad4d0b7fa2e8b2635bfae58ddcf4b0e
Khoa <59268707+akn0717@users.noreply.github.com>
2022-08-03 00:44:19 -0700

    hot fix

--- app/src/main/java/com/AERYZ/treasurefind/main/ui/test/TestFragment.kt
+++ app/src/main/java/com/AERYZ/treasurefind/main/ui/test/TestFragment.kt
@@ -40,7 +40,7 @@ class TestFragment : Fragment() {
             startActivity(intent)
         }
 
-        //Map Activity button
+        //Treasure adding button
         val btn_treasureAdding: Button = root.findViewById(R.id.btn_treasureadding)
         btn_treasureAdding.setOnClickListener() {
             val intent = Intent(requireContext(), TreasureAddingActivity::class.java)
@@ -52,6 +52,8 @@ class TestFragment : Fragment() {
         btn_SRAdding.setOnClickListener() {
             val intent = Intent(requireContext(), SRActivity::class.java)
             intent.putExtra(MapsActivity.tid_KEY, "Qqt7DNLsk12CWOtK26uE")
+            startActivity(intent)
+        }
             
 
         return root
