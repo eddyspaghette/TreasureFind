8f6ec9327c72093a871e1b01ad8a80a03a0ef98e
Khoa <59268707+akn0717@users.noreply.github.com>
2022-08-05 16:01:59 -0700

    temp

--- app/src/main/java/com/AERYZ/treasurefind/main/ui/seeker_map/SeekerMapActivity.kt
+++ app/src/main/java/com/AERYZ/treasurefind/main/ui/seeker_map/SeekerMapActivity.kt
@@ -32,7 +32,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.firebase.auth.FirebaseAuth
 
-class SeekerMapActivity : AppCompatActivity(), OnMapReadyCallback, ImageReader.OnImageAvailableListener {
+class SeekerMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivitySeekermapBinding
@@ -116,13 +116,7 @@ class SeekerMapActivity : AppCompatActivity(), OnMapReadyCallback, ImageReader.O
 
         })
 
-        //live camera
-        setFragment()
-        val btn_capture: Button = findViewById(R.id.btn_capture)
-        btn_capture.setOnLongClickListener() {
-            isCapture = 1
-            true
-        }
+
 
 
     }
@@ -196,144 +190,6 @@ class SeekerMapActivity : AppCompatActivity(), OnMapReadyCallback, ImageReader.O
         stopService(serviceIntent)
     }
 
-    //live camera
-    var previewHeight = 0;
-    var previewWidth = 0
-    var sensorOrientation = 0;
-    var isCapture = 0
 
-    //TODO getting frames of live camera footage and passing them to model
-    private var isProcessingFrame = false
-    private val yuvBytes = arrayOfNulls<ByteArray>(3)
-    private var rgbBytes: IntArray? = null
-    private var yRowStride = 0
-    private var postInferenceCallback: Runnable? = null
-    private var imageConverter: Runnable? = null
-    private var rgbFrameBitmap: Bitmap? = null
-
-    //TODO fragment which show llive footage from camera
-    protected fun setFragment() {
-        val manager =
-            getSystemService(Context.CAMERA_SERVICE) as CameraManager
-        var cameraId: String? = null
-        try {
-            cameraId = manager.cameraIdList[0]
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-        }
-        val fragment: Fragment
-        val camera2Fragment = CameraConnectionFragment.newInstance(
-            object :
-                CameraConnectionFragment.ConnectionCallback {
-                override fun onPreviewSizeChosen(size: Size?, rotation: Int) {
-                    previewHeight = size!!.height
-                    previewWidth = size.width
-                    sensorOrientation = rotation - getScreenOrientation()
-                }
-            },
-            this,
-            R.layout.fragment_camera,
-            Size(480, 480)
-        )
-        camera2Fragment.setCamera(cameraId)
-        fragment = camera2Fragment
-        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
-    }
-
-    protected fun getScreenOrientation(): Int {
-        return when (windowManager.defaultDisplay.rotation) {
-            Surface.ROTATION_270 -> 270
-            Surface.ROTATION_180 -> 180
-            Surface.ROTATION_90 -> 90
-            else -> 0
-        }
-    }
-
-
-
-    //TODO getting frames of live camera footage and passing them to model
-    override fun onImageAvailable(reader: ImageReader) {
-        if (isCapture == 1) {
-            cameraprocess(reader)
-            //action with output image here
-            isCapture = 0
-            if (rgbFrameBitmap != null) {
-                val bitmap = Util.rotateBitmap(rgbFrameBitmap!!, 90f)
-                val uid = FirebaseAuth.getInstance().uid
-                val sR = SR(uid!!, bitmap!!)
-                myFirebase.updateSR(tid, sR)
-                Toast.makeText(this, "Uploaded!", Toast.LENGTH_SHORT).show()
-            }
-        }
-        val temp = reader.acquireLatestImage()
-        if (temp!= null)
-        {
-            temp.close()
-        }
-    }
-
-
-    private fun cameraprocess(reader: ImageReader) {
-        if (previewWidth != 0 && previewHeight != 0) {
-            if (rgbBytes == null) {
-                rgbBytes = IntArray(previewWidth * previewHeight)
-            }
-            try {
-                val image = reader.acquireLatestImage() ?: return
-                if (!isProcessingFrame) {
-                    isProcessingFrame = true
-                    val planes = image.planes
-                    fillBytes(planes, yuvBytes)
-                    yRowStride = planes[0].rowStride
-                    val uvRowStride = planes[1].rowStride
-                    val uvPixelStride = planes[1].pixelStride
-                    imageConverter = Runnable {
-                        ImageUtils.convertYUV420ToARGB8888(
-                            yuvBytes[0]!!,
-                            yuvBytes[1]!!,
-                            yuvBytes[2]!!,
-                            previewWidth,
-                            previewHeight,
-                            yRowStride,
-                            uvRowStride,
-                            uvPixelStride,
-                            rgbBytes!!
-                        )
-                    }
-                    postInferenceCallback = Runnable {
-                        image.close()
-                        isProcessingFrame = false
-                    }
-                    processImage()
-                } else {
-                    image.close()
-                }
-            } catch (e: Exception) {
-                return
-            }
-        }
-    }
-
-    private fun processImage() {
-        imageConverter!!.run()
-        rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Bitmap.Config.ARGB_8888)
-        rgbFrameBitmap?.setPixels(rgbBytes, 0, previewWidth, 0, 0, previewWidth, previewHeight)
-        postInferenceCallback!!.run()
-    }
-
-    protected fun fillBytes(
-        planes: Array<Image.Plane>,
-        yuvBytes: Array<ByteArray?>
-    ) {
-        // Because of the variable row stride it's not possible to know in
-        // advance the actual necessary dimensions of the yuv planes.
-        for (i in planes.indices) {
-            val buffer = planes[i].buffer
-            if (yuvBytes[i] == null) {
-                yuvBytes[i] = ByteArray(buffer.capacity())
-            }
-            buffer[yuvBytes[i]]
-        }
-    }
 
 }
---
+++ app/src/main/java/com/AERYZ/treasurefind/main/ui/seeker_map/SeekerSubmitFragment.kt
@@ -0,0 +1,191 @@
+package com.AERYZ.treasurefind.main.ui.seeker_map
+
+import android.content.Context.CAMERA_SERVICE
+import android.graphics.Bitmap
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
+import android.media.Image
+import android.media.ImageReader
+import android.os.Bundle
+import android.util.Size
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Surface
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+import com.AERYZ.treasurefind.R
+import com.AERYZ.treasurefind.db.SR
+import com.AERYZ.treasurefind.main.ui.livecamera.CameraConnectionFragment
+import com.AERYZ.treasurefind.main.ui.livecamera.ImageUtils
+import com.AERYZ.treasurefind.main.util.Util
+import com.google.firebase.auth.FirebaseAuth
+
+
+class SeekerSubmitFragment : Fragment(), ImageReader.OnImageAvailableListener {
+    private lateinit var
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_seeker_submit, container, false)
+
+        //live camera
+        setFragment()
+        val btn_capture: Button = view.findViewById(R.id.btn_capture)
+        btn_capture.setOnLongClickListener() {
+            isCapture = 1
+            true
+        }
+
+        return view
+    }
+
+    //live camera
+    var previewHeight = 0;
+    var previewWidth = 0
+    var sensorOrientation = 0;
+    var isCapture = 0
+
+    //TODO getting frames of live camera footage and passing them to model
+    private var isProcessingFrame = false
+    private val yuvBytes = arrayOfNulls<ByteArray>(3)
+    private var rgbBytes: IntArray? = null
+    private var yRowStride = 0
+    private var postInferenceCallback: Runnable? = null
+    private var imageConverter: Runnable? = null
+    private var rgbFrameBitmap: Bitmap? = null
+
+    //TODO fragment which show llive footage from camera
+    protected fun setFragment() {
+        val manager = getSystemService(requireContext(), CAMERA_SERVICE) as CameraManager
+        var cameraId: String? = null
+        try {
+            cameraId = manager.cameraIdList[0]
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+        val fragment: android.app.Fragment
+        val camera2Fragment = CameraConnectionFragment.newInstance(
+            object :
+                CameraConnectionFragment.ConnectionCallback {
+                override fun onPreviewSizeChosen(size: Size?, rotation: Int) {
+                    previewHeight = size!!.height
+                    previewWidth = size.width
+                    sensorOrientation = rotation - getScreenOrientation()
+                }
+            },
+            this,
+            R.layout.fragment_camera,
+            Size(480, 480)
+        )
+        camera2Fragment.setCamera(cameraId)
+        fragment = camera2Fragment
+        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+    }
+
+    protected fun getScreenOrientation(): Int {
+        return when (windowManager.defaultDisplay.rotation) {
+            Surface.ROTATION_270 -> 270
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_90 -> 90
+            else -> 0
+        }
+    }
+
+
+
+    //TODO getting frames of live camera footage and passing them to model
+    override fun onImageAvailable(reader: ImageReader) {
+        if (isCapture == 1) {
+            cameraprocess(reader)
+            //action with output image here
+            isCapture = 0
+            if (rgbFrameBitmap != null) {
+                val bitmap = Util.rotateBitmap(rgbFrameBitmap!!, 90f)
+                val uid = FirebaseAuth.getInstance().uid
+                val sR = SR(uid!!, bitmap!!)
+                myFirebase.updateSR(tid, sR)
+                Toast.makeText(this, "Uploaded!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val temp = reader.acquireLatestImage()
+        if (temp!= null)
+        {
+            temp.close()
+        }
+    }
+
+
+    private fun cameraprocess(reader: ImageReader) {
+        if (previewWidth != 0 && previewHeight != 0) {
+            if (rgbBytes == null) {
+                rgbBytes = IntArray(previewWidth * previewHeight)
+            }
+            try {
+                val image = reader.acquireLatestImage() ?: return
+                if (!isProcessingFrame) {
+                    isProcessingFrame = true
+                    val planes = image.planes
+                    fillBytes(planes, yuvBytes)
+                    yRowStride = planes[0].rowStride
+                    val uvRowStride = planes[1].rowStride
+                    val uvPixelStride = planes[1].pixelStride
+                    imageConverter = Runnable {
+                        ImageUtils.convertYUV420ToARGB8888(
+                            yuvBytes[0]!!,
+                            yuvBytes[1]!!,
+                            yuvBytes[2]!!,
+                            previewWidth,
+                            previewHeight,
+                            yRowStride,
+                            uvRowStride,
+                            uvPixelStride,
+                            rgbBytes!!
+                        )
+                    }
+                    postInferenceCallback = Runnable {
+                        image.close()
+                        isProcessingFrame = false
+                    }
+                    processImage()
+                } else {
+                    image.close()
+                }
+            } catch (e: Exception) {
+                return
+            }
+        }
+    }
+
+    private fun processImage() {
+        imageConverter!!.run()
+        rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Bitmap.Config.ARGB_8888)
+        rgbFrameBitmap?.setPixels(rgbBytes, 0, previewWidth, 0, 0, previewWidth, previewHeight)
+        postInferenceCallback!!.run()
+    }
+
+    protected fun fillBytes(
+        planes: Array<Image.Plane>,
+        yuvBytes: Array<ByteArray?>
+    ) {
+        // Because of the variable row stride it's not possible to know in
+        // advance the actual necessary dimensions of the yuv planes.
+        for (i in planes.indices) {
+            val buffer = planes[i].buffer
+            if (yuvBytes[i] == null) {
+                yuvBytes[i] = ByteArray(buffer.capacity())
+            }
+            buffer[yuvBytes[i]]
+        }
+    }
+
+}
