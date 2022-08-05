73fbaf8b129e35f2feeb2c9f8dae366383661fe7
Khoa <59268707+akn0717@users.noreply.github.com>
2022-08-04 20:54:03 -0700

    fix null upload && fix maps

--- app/src/main/java/com/AERYZ/treasurefind/main/ui/hider_place/HiderPlaceViewModel.kt
+++ app/src/main/java/com/AERYZ/treasurefind/main/ui/hider_place/HiderPlaceViewModel.kt
@@ -1,10 +1,11 @@
 package com.AERYZ.treasurefind.main.ui.hider_place
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.lifecycle.MutableLiveData
+import com.AERYZ.treasurefind.R
 import com.AERYZ.treasurefind.main.services.ServiceViewModel
 
 class HiderPlaceViewModel : ServiceViewModel() {
     var treasurePhoto = MutableLiveData<Bitmap>()
-
 }
