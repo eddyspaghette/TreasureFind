6b3e93041561dd90b1b4a50d05d7a8fb59fe8ca0
zichub <bzclovetx@gmail.com>
2022-08-04 19:32:08 -0700

    back button added on activities

--- app/src/main/java/com/AERYZ/treasurefind/main/ui/bottomsheet/BottomSheetActivity.kt
+++ app/src/main/java/com/AERYZ/treasurefind/main/ui/bottomsheet/BottomSheetActivity.kt
@@ -8,6 +8,7 @@ class BottomSheetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_sheet)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
     }
 }
