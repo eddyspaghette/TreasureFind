5688fe55824533d8dcc1d50ad0a734ee040c9f50
Edward Zhou <eddyspaghette@gmail.com>
2022-08-07 03:48:24 -0700

    add loading image button to sr activity

--- app/src/main/java/com/AERYZ/treasurefind/main/ui/seeker_map/SeekerSubmitFragment.kt
+++ app/src/main/java/com/AERYZ/treasurefind/main/ui/seeker_map/SeekerSubmitFragment.kt
@@ -1,19 +1,21 @@
 package com.AERYZ.treasurefind.main.ui.seeker_map
 
+import android.annotation.SuppressLint
 import android.content.Context.CAMERA_SERVICE
 import android.content.Context.WINDOW_SERVICE
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraManager
 import android.media.Image
 import android.media.ImageReader
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.util.Size
 import android.view.*
-import android.widget.Button
 import android.widget.Toast
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.AERYZ.treasurefind.R
@@ -24,15 +26,24 @@ import com.AERYZ.treasurefind.main.ui.livecamera.ImageUtils
 import com.AERYZ.treasurefind.main.util.Util
 import com.AERYZ.treasurefind.main.util.Util.calculateDistance
 import com.google.android.gms.maps.model.LatLng
+import com.apachat.loadingbutton.core.customViews.CircularProgressButton
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.concurrent.schedule
 
 
+@SuppressLint("ClickableViewAccessibility")
 class SeekerSubmitFragment : Fragment(), ImageReader.OnImageAvailableListener {
 
     private var myFirebase = MyFirebase()
     private var tid = ""
     private lateinit var mapViewModel: SeekerMapViewModel
     private lateinit var mapViewModelFactory: SeekerMapViewModelFactory
+    private lateinit var btn_capture: CircularProgressButton
+    private lateinit var handler: Handler
 
 
     override fun onCreateView(
@@ -52,9 +63,27 @@ class SeekerSubmitFragment : Fragment(), ImageReader.OnImageAvailableListener {
 
         //live camera
         setFragment()
-        val btn_capture: Button = view.findViewById(R.id.btn_capture)
-        btn_capture.setOnLongClickListener() {
+
+        handler = Handler(Looper.getMainLooper())
+        val runnable = Runnable() {
             isCapture = 1
+        }
+        btn_capture = view.findViewById(R.id.btn_capture)
+        btn_capture.setOnTouchListener { _, event ->
+            when(event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    btn_capture.startAnimation()
+                    handler.postDelayed(runnable, 2400)
+                }
+                MotionEvent.ACTION_UP -> {
+                    btn_capture.revertAnimation()
+                    handler.removeCallbacks(runnable)
+                }
+                MotionEvent.ACTION_CANCEL -> {
+                    btn_capture.revertAnimation()
+                    handler.removeCallbacks(runnable)
+                }
+            }
             true
         }
 
@@ -62,9 +91,9 @@ class SeekerSubmitFragment : Fragment(), ImageReader.OnImageAvailableListener {
     }
 
     //live camera
-    var previewHeight = 0;
+    var previewHeight = 0
     var previewWidth = 0
-    var sensorOrientation = 0;
+    var sensorOrientation = 0
     var isCapture = 0
 
     //TODO getting frames of live camera footage and passing them to model
@@ -115,7 +144,6 @@ class SeekerSubmitFragment : Fragment(), ImageReader.OnImageAvailableListener {
     }
 
 
-
     //TODO getting frames of live camera footage and passing them to model
     override fun onImageAvailable(reader: ImageReader) {
         if (isCapture == 1) {
@@ -136,12 +164,28 @@ class SeekerSubmitFragment : Fragment(), ImageReader.OnImageAvailableListener {
                 else {
                     //TODO: Dialog of failed submit (Too far from the treasure)
                     Toast.makeText(activity, "Too far from the treasure", Toast.LENGTH_SHORT).show()
+                    val runnable = Runnable() {
+                        btn_capture.revertAnimation {
+                            btn_capture.text = "Cooldown (5s)"
+                            btn_capture.isEnabled = false
+                        }
+                    }
+                    val runnable2 = Runnable() {
+                        btn_capture.startAnimation()
+                        btn_capture.revertAnimation {
+                            btn_capture.text = getString(R.string.capture)
+                            btn_capture.isEnabled = true
+                            btn_capture.initialCorner = 15.toFloat()
+                        }
+                    }
+                    handler.post(runnable)
+                    handler.postDelayed(runnable2, 5000)
                 }
             }
         }
-        val temp = reader.acquireLatestImage()
-        temp?.close()
+        reader.acquireLatestImage()?.close()
     }
+
 
 
     private fun cameraprocess(reader: ImageReader) {
@@ -205,6 +249,11 @@ class SeekerSubmitFragment : Fragment(), ImageReader.OnImageAvailableListener {
             }
             buffer[yuvBytes[i]]
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        btn_capture.dispose()
     }
 
 }
