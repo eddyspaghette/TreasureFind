557aa7ef45db26ef432467488c871ccb04c16cec
DELL6288 <yuchun@email.com>
2022-08-07 04:12:40 -0700

    finished clock timer animation

--- app/src/main/java/com/AERYZ/treasurefind/main/ui/dialogs/TimerDialog.kt
+++ app/src/main/java/com/AERYZ/treasurefind/main/ui/dialogs/TimerDialog.kt
@@ -1,4 +1,25 @@
 package com.AERYZ.treasurefind.main.ui.dialogs
 
+import android.app.Dialog
+import android.content.Context
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.view.LayoutInflater
+import android.widget.ImageView
+import com.AERYZ.treasurefind.R
+
 class TimerDialog {
+    companion object {
+        fun clockDialog(context: Context): Dialog {
+            val dialog = Dialog(context)
+            val view = LayoutInflater.from(context).inflate(R.layout.dialog_timer, null)
+            dialog.setContentView(view)
+            dialog.setCancelable(false)
+            dialog.window!!.setBackgroundDrawableResource(R.drawable.dialog_rounded_background)
+            val clockView: ImageView = view.findViewById(R.id.clockVector)
+            val drawable = clockView.drawable
+            val animation = drawable as AnimatedVectorDrawable
+            animation.start()
+            return dialog
+        }
+    }
 }
